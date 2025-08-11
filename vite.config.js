6346f2ea97b0c5d71bc6faddc7dab78de264b243
Evelyn Chevalier <velocity.toml@proton.me>
2025-08-11 22:59:03 +0700

    2nd attempt fixing vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { join, dirname, resolve } from "path";
+import { join, dirname } from "path";
 import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
+import * as dotenv from 'dotenv';
 
 const path = fileURLToPath(import.meta.url);
+dotenv.config();
+
+const allowedHosts = process.env.VITE_ALLOWED_HOSTS ?
+  process.env.VITE_ALLOWED_HOSTS.split(',') : [];
 
 export default {
   root: join(dirname(path), "client"),
   plugins: [react()],
   server: {
-    allowedHosts: ["*"]
+    allowedHosts: allowedHosts
   }
 };
