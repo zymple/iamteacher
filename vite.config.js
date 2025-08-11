004e33b7b51d599df8c0cbfb95c3cb4aeaf369a0
Phapoom Saksri <blueskychan@techtransthai.org>
2025-08-11 23:17:27 +0700

    Fixing allowedhost hard code, login api messed up, improve .env and debug stuff.

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
 
 const path = fileURLToPath(import.meta.url);
+const allowedHosts = (process.env.allowedHosts || "localhost")
+  .split(",")
+  .map(h => h.trim());
 
 export default {
   root: join(dirname(path), "client"),
   plugins: [react()],
   server: {
-    allowedHosts: ["iamteacher.techtransthai.org"]
+    allowedHosts: allowedHosts
   }
 };
